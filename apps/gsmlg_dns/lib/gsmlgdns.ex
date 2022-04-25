221d50d505c321b3d25db0acc5554ac973312871
Jonathan Gao <gsmlg.com@gmail.com>
2022-04-25 15:01:09 +0800

    feat: Initialize GSMLGDNS.

---
+++ apps/gsmlg_dns/lib/gsmlgdns.ex
@@ -0,0 +1,18 @@
+defmodule GSMLGDNS do
+  @moduledoc """
+  Documentation for `GSMLGDNS`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> GSMLGDNS.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ apps/gsmlg_dns/test/gsmlgdns_test.exs
@@ -0,0 +1,8 @@
+defmodule GSMLGDNSTest do
+  use ExUnit.Case
+  doctest GSMLGDNS
+
+  test "greets the world" do
+    assert GSMLGDNS.hello() == :world
+  end
+end
