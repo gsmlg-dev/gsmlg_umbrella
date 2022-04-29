f3361a600df72a89439df95ab254c30bdac12fc8
Jonathan Gao <gsmlg.com@gmail.com>
2022-04-30 00:11:57 +0800

    fix: Fix components.

--- apps/gsmlg_web/assets/js/components/index.js
+++ apps/gsmlg_web/assets/js/components/index.js
@@ -1,15 +1,3 @@
-import '@gsmlg/ui5-webcomponents/dist/TableCell.js';
-import '@gsmlg/ui5-webcomponents/dist/TableColumn.js';
-import '@gsmlg/ui5-webcomponents/dist/TableGroupRow.js';
-import '@gsmlg/ui5-webcomponents/dist/Table.js';
-import '@gsmlg/ui5-webcomponents/dist/TableRow.js';
-import '@gsmlg/ui5-webcomponents/dist/DatePicker.js';
-import "@gsmlg/ui5-webcomponents/dist/DateTimePicker.js";
-import '@gsmlg/ui5-webcomponents/dist/Label.js';
-import "@gsmlg/ui5-webcomponents/dist/Title.js";
-import "@gsmlg/ui5-webcomponents/dist/features/InputElementsFormSupport.js";
-
-
 // import { setTheme } from "@gsmlg/ui5-webcomponents-base/dist/config/Theme.js";
 // setTheme("sap_belize_hcb");
 
