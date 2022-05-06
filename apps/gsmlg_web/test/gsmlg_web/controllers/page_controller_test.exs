fe8d89b8055b7786b6d1e1c4d32d9fdec53f602d
Jonathan Gao <gsmlg.com@gmail.com>
2022-05-06 16:15:39 +0800

    chore: Fix test page controller.

--- apps/gsmlg_web/test/gsmlg_web/controllers/page_controller_test.exs
+++ apps/gsmlg_web/test/gsmlg_web/controllers/page_controller_test.exs
@@ -1,8 +1,8 @@
 defmodule GSMLGWeb.PageControllerTest do
   use GSMLGWeb.ConnCase
 
-  test "GET /", %{conn: conn} do
-    conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "GSMLG"
+  test "GET /not_found", %{conn: conn} do
+    conn = get(conn, "/not_found")
+    assert html_response(conn, 404) =~ "BITW"
   end
 end
