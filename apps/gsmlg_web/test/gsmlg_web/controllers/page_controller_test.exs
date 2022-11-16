c9940d4c0f3e3ef9ffeb99e454db0ca16abbcab7
Jonathan Gao <gsmlg.com@gmail.com>
2022-11-16 18:07:29 +0800

    fix: Fix test.

--- apps/gsmlg_web/test/gsmlg_web/controllers/page_controller_test.exs
+++ apps/gsmlg_web/test/gsmlg_web/controllers/page_controller_test.exs
@@ -1,8 +1,13 @@
 defmodule GSMLGWeb.PageControllerTest do
   use GSMLGWeb.ConnCase
 
+  test "GET /", %{conn: conn} do
+    conn = get(conn, "/")
+    assert html_response(conn, 200) =~ "Home"
+  end
+
   test "GET /not_found", %{conn: conn} do
     conn = get(conn, "/not_found")
-    assert html_response(conn, 404) =~ "BITW"
+    assert html_response(conn, 404) =~ "404"
   end
 end
