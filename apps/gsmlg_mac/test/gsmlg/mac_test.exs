a935bbe2cc311fd6eb722a67b369a8e1d26a2fde
Jonathan Gao <gsmlg.com@gmail.com>
2022-12-10 11:11:14 +0800

    fix: Fix test.

--- apps/gsmlg_mac/test/gsmlg/mac_test.exs
+++ apps/gsmlg_mac/test/gsmlg/mac_test.exs
@@ -3,6 +3,7 @@ defmodule GSMLG.MACTest do
   doctest GSMLG.MAC
 
   test "greets the world" do
-    assert GSMLG.MAC.hello() == :world
+    assert GSMLG.MAC.lookup_vendor("00:00:0A:BB:28:FC") ==
+             {:ok, "OmronTat", "Omron Tateisi Electronics Co."}
   end
 end
