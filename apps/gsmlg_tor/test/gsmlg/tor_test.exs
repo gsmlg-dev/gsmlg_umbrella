23e7d855499e17249fc78fdcec7ac6ae03489411
Jonathan Gao <gsmlg.com@gmail.com>
2023-03-07 23:29:56 +0800

    fix: Fix test issue.

--- apps/gsmlg_tor/test/gsmlg/tor_test.exs
+++ apps/gsmlg_tor/test/gsmlg/tor_test.exs
@@ -2,7 +2,8 @@ defmodule GSMLG.TorTest do
   use ExUnit.Case
   doctest GSMLG.Tor
 
-  test "test tor download url" do
+  test "test generate tor default config" do
+    GSMLG.Tor.Config.init()
     assert GSMLG.Tor.get_torrc() =~ "SOCKSPort 127.0.0.1:9050"
   end
 end
