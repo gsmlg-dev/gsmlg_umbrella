7ab06c5baa22de4f4ebe9a604495b2796d392f4c
Jonathan Gao <gsmlg.com@gmail.com>
2023-03-15 23:35:13 +0800

    fix: Publish GSMLG.Socket.

--- apps/gsmlg_socket/mix.exs
+++ apps/gsmlg_socket/mix.exs
@@ -9,8 +9,11 @@ defmodule GSMLG.Socket.MixProject do
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
       lockfile: "../../mix.lock",
-      elixir: "~> 1.13",
+      elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
+      name: "GSMLG.Socket",
+      description: "GSMLG specify Socket connection package",
+      package: package(),
       deps: deps()
     ]
   end
@@ -29,6 +32,17 @@ defmodule GSMLG.Socket.MixProject do
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
       # {:sibling_app_in_umbrella, in_umbrella: true}
+    ]
+  end
+
+  defp package do
+    [
+      maintainers: ["Jonathan Gao"],
+      licenses: ["MIT"],
+      files: ~w(lib LICENSE mix.exs README.md),
+      links: %{
+        Changelog: "https://hexdocs.pm/gsmlg_socket/changelog.html"
+      }
     ]
   end
 end
