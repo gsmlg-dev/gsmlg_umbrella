cc07c1a7479653bc60897828365019825780f87e
Jonathan Gao <gsmlg.com@gmail.com>
2023-04-04 16:54:13 +0800

    chore: Remove.

--- apps/gsmlg_command_platform/test/gsmlg/command_platform_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule GSMLG.CommandPlatformTest do
-  use ExUnit.Case
-  doctest GSMLG.CommandPlatform
-
-  test "greets the world" do
-    assert GSMLG.CommandPlatform.hello() == :world
-  end
-end
