2778aa6d43dd4b0cbe783bdb3235cba3698bbd66
Jonathan Gao <gsmlg.com@gmail.com>
2024-10-03 01:01:09 +0800

    fix: Fix.

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule GSMLG.Umbrella.MixProject do
   def project do
     [
       apps_path: "apps",
-      version: "0.1.0",
+      version: "1.0.0",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       aliases: aliases(),
@@ -21,47 +21,27 @@ defmodule GSMLG.Umbrella.MixProject do
             gsmlg_web: :permanent
           ]
         ],
-        gsmlg_admin_web_only: [
-          applications: [gsmlg_admin_web: :permanent]
-        ],
-        gsmlg_web_only: [
-          applications: [gsmlg_web: :permanent]
-        ],
-        gsmlg_only: [
-          applications: [gsmlg: :permanent]
+        gsmlg_umbrella_app: [
+          steps: [:assemble, &Burrito.wrap/1],
+          burrito: [
+            targets: [
+              linux_arm64: [os: :linux, cpu: :aarch64],
+              linux_amd64: [os: :linux, cpu: :x86_64],
+            ]
+          ]
         ]
       ]
     ]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options.
-  #
-  # Dependencies listed here are available only for this project
-  # and cannot be accessed from applications inside the apps/ folder.
   defp deps do
     [
+      {:burrito, "~> 1.0"},
       {:phoenix, "~> 1.7", override: true},
       {:phoenix_live_view, "~> 0.20"}
     ]
   end
 
-  # Aliases are shortcuts or tasks specific to the current project.
-  # For example, to install project dependencies and perform other setup tasks, run:
-  #
-  #     $ mix setup
-  #
-  # See the documentation for `Mix` for more info on aliases.
-  #
-  # Aliases listed here are available only for this project
-  # and cannot be accessed from applications inside the apps/ folder.
   defp aliases do
     [
       # run `mix setup` in all child apps
