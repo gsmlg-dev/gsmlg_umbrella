7ce88c4a732eacf41756f9097c378536ec37c65d
Jonathan Gao <gsmlg.com@gmail.com>
2025-02-06 23:26:42 +0800

    fix: Fix flake.nix.

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/release-24.11";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
