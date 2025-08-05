81f5395a558ec642447fed1665911c921203fd2e
Jonathan Gao <gsmlg.com@gmail.com>
2025-08-05 23:36:06 +0800

    refacotr: Use GSMLG.Config.

--- apps/gsmlg_config/lib/gsmlg/config/setup.ex
+++ apps/gsmlg_config/lib/gsmlg/config/setup.ex
@@ -39,12 +39,12 @@ defmodule GSMLG.Config.Setup do
 
     if config[:mnesia_dir] != nil do
       mnesia_dir = config[:mnesia_dir] |> String.to_charlist()
-      Application.put_env(:mnesia, :dir, mnesia_dir)
+      update_env(:mnesia, :dir, mnesia_dir)
     end
   end
 
   def setup_database(config) do
-    Application.put_env(:gsmlg, GSMLG.Repo,
+    update_env(:gsmlg, GSMLG.Repo,
       username: config[:username],
       password: config[:password],
       hostname: config[:hostname],
@@ -58,7 +58,7 @@ defmodule GSMLG.Config.Setup do
   def setup_web(config) do
     uri = URI.parse(config[:url])
 
-    Application.put_env(:gsmlg_web, GSMLG.Web.Endpoint,
+    update_env(:gsmlg_web, GSMLG.Web.Endpoint,
       secret_key_base: config[:secret_key_base],
       http: [
         ip: {0, 0, 0, 0, 0, 0, 0, 0},
@@ -75,7 +75,8 @@ defmodule GSMLG.Config.Setup do
   def setup_admin_web(config) do
     uri = URI.parse(config[:url])
 
-    Application.put_env(:gsmlg_admin_web, GSMLG.AdminWeb.Endpoint,
+    update_env(:gsmlg_admin_web, GSMLG.AdminWeb.Endpoint,
+      adapter: Bandit.PhoenixAdapter,
       secret_key_base: config[:secret_key_base],
       http: [
         ip: {0, 0, 0, 0, 0, 0, 0, 0},
@@ -84,12 +85,11 @@ defmodule GSMLG.Config.Setup do
       server: config[:server] != false,
       url: [host: uri.host, port: uri.port, scheme: uri.scheme, path: uri.path],
       user_register: config[:user_register] == true,
-      commander_platform_key: config[:commander_platform_key]
     )
   end
 
   def setup_couchdb(config) do
-    Application.put_env(:gsmlg_couchdb, GSMLG.CouchDB.Connection,
+    update_env(:gsmlg_couchdb, GSMLG.CouchDB.Connection,
       host: config[:host],
       port: config[:port],
       username: config[:username],
@@ -98,7 +98,7 @@ defmodule GSMLG.Config.Setup do
   end
 
   def setup_commander(config) do
-    Application.put_env(:gsmlg_commander, GSMLG.Commander,
+    update_env(:gsmlg_commander, GSMLG.Commander,
       name: config[:name],
       platform_url: config[:platform_url],
       platform_key: config[:platform_key]
@@ -115,10 +115,32 @@ defmodule GSMLG.Config.Setup do
   end
 
   def setup_web_push(config) do
-    Application.put_env(:gsmlg_web_push, :vapid_details,
+    update_env(:gsmlg_web_push, :vapid_details,
       subject: config[:subject],
       public_key: config[:public_key],
       private_key: config[:private_key]
     )
   end
+
+
+  def update_env(app, key, new_value) do
+    current = Application.get_env(app, key, [])
+    merged = deep_merge(current, new_value)
+    Application.put_env(app, key, merged)
+  end
+
+  defp deep_merge(old, new) when is_list(old) and is_list(new) do
+    Keyword.merge(old, new, fn _key, val1, val2 ->
+      deep_merge(val1, val2)
+    end)
+  end
+
+  defp deep_merge(%{} = old, %{} = new) do
+    Map.merge(old, new, fn _key, val1, val2 ->
+      deep_merge(val1, val2)
+    end)
+  end
+
+  defp deep_merge(_old, new), do: new
+
 end
