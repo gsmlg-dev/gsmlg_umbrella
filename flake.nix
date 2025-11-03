130c8bb7a67a9c9b8969cac93197c878a4011a04
Jonathan Gao <gsmlg.com@gmail.com>
2025-11-03 13:26:53 +0800

    chore: update nixpkgs to release-25.05
    - Update nixpkgs reference from release-24.11 to release-25.05
    - Update flake-utils to latest version
    - Update all dependencies in flake.lock

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/release-24.11";
+    nixpkgs.url = "github:nixos/nixpkgs/release-25.05";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
