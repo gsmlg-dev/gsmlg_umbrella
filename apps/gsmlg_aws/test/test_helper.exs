77674c5c29f42ede5b38d795edb44530d267c2a9
Jonathan Gao <gsmlg.com@gmail.com>
2026-01-09 16:39:11 +0800

    fix: prevent double-start of GSMLG.AWS in test_helper
    When running tests from umbrella root, GSMLG.Application already starts
    GSMLG.AWS. The test_helper was trying to start it again, causing Cachex
    duplicate registration error.

--- apps/gsmlg_aws/test/test_helper.exs
+++ apps/gsmlg_aws/test/test_helper.exs
@@ -1,2 +1,7 @@
 ExUnit.start()
-GSMLG.AWS.start_link([])
+# Note: GSMLG.AWS is started by GSMLG.Application, no need to start it here
+# when running tests from the umbrella root. Only start if not already running.
+case Process.whereis(GSMLG.AWS.Finch) do
+  nil -> GSMLG.AWS.start_link([])
+  _pid -> :ok
+end
