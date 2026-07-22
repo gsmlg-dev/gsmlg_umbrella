0b6aa3f422536bc495128ad7e07f16c5885782f5
Jonathan Gao <gsmlg.com@gmail.com>
2026-07-22 12:34:13 +0800

    chore(proxy-rules): scaffold umbrella application

---
+++ apps/proxy_rules/mix.exs
@@ -0,0 +1,31 @@
+defmodule GSMLG.ProxyRules.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :proxy_rules,
+      version: "0.1.0",
+      build_path: "../../_build",
+      config_path: "../../config/config.exs",
+      deps_path: "../../deps",
+      lockfile: "../../mix.lock",
+      elixir: "~> 1.18",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  def application do
+    [
+      mod: {GSMLG.ProxyRules.Application, []},
+      extra_applications: [:logger]
+    ]
+  end
+
+  defp deps do
+    [
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false}
+    ]
+  end
+end
